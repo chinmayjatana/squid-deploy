8a2e6a9a20657cd27063daf17c52db6509bfe4dc
chinmayjatana <chinmayjatana@gmail.com>
2024-04-24 12:59:29 +0530

    Update deploy_squid_proxy.sh

--- deploy_squid_proxy.sh
+++ deploy_squid_proxy.sh
@@ -7,6 +7,12 @@ NAMESPACE=proxy-ns
 CONFIG_NAME=squid-config
 CONFIG_FILE=squid.conf
 
+if  [[ "$DISTRIBUTION" == "Openshift" ]] || [[ "$DISTRIBUTION" == "TKGM" ]] || [[ "$DISTRIBUTION" == "TKGS" ]] || [[ "$DISTRIBUTION" == *"TKGI"* ]]; then
+  SQUID_IMAGE="harbor-repo.vmware.com/dockerhub-proxy-cache/sameersbn/squid:3.5.27-2"
+else
+  SQUID_IMAGE="sameersbn/squid:3.5.27-2"
+fi
+
 # Create Namespace
 kubectl create ns $NAMESPACE
 
@@ -32,7 +38,7 @@ spec:
     spec:
       containers:
       - name: squid
-        image: harbor-repo.vmware.com/dockerhub-proxy-cache/sameersbn/squid:3.5.27-2
+        image: $SQUID_IMAGE
         ports:
         - containerPort: 3128
         volumeMounts:
@@ -60,4 +66,7 @@ spec:
     app: squid
 EOF
 
+echo "Waiting for 10 sec for squid pod to come up and 2 min for squid pod to be running"
+sleep 10
+kubectl wait --for=condition=ready --timeout=120s pod -l app=squid -n $NAMESPACE
 echo "Squid Proxy Deployment Completed. Use DNS name: ${SERVICE_NAME}.${NAMESPACE}.svc.cluster.local:3128"
